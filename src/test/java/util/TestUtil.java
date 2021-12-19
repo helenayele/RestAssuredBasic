3241e056e14fe8db1d7dc2fe6ddcbe411715a27d
Helen <ahelen19@gmail.com>
2021-12-19 17:37:23 +0300

    add new reset assured basic project

---
+++ src/test/java/util/TestUtil.java
@@ -0,0 +1,18 @@
+package util;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+
+public class TestUtil {
+    //Verify the http response status returned. Check Status Code is 200?
+    public void checkStatusIs200(Response res) {
+        Assert.assertEquals(res.getStatusCode(), 200, "Status Check Failed!");
+    }
+    //Get Clients
+    public <T> ArrayList<T> getClients(JsonPath jp) {
+        return jp.get();
+    }
+}
